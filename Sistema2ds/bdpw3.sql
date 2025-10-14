80c39ce91c9d82e11b10163ade626b04e7559d79
juliajujute <juliatenorio_am@icloud.com>
2025-10-13 22:31:41 -0300

    Add files via upload
    com o banco

---
+++ Sistema2ds/bdpw3.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE bdpw3;
+
+USE bdpw3;
+
+CREATE TABLE demo(
+	 id INT AUTO_INCREMENT PRIMARY KEY,
+	 name VARCHAR (80),
+     idade INT (3),
+	 cpf VARCHAR (11),
+     abrir VARCHAR (40),
+     dobrar VARCHAR (40)
+    );
+    SELECT * FROM demo;
